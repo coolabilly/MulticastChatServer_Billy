0a8639b0777f0679eda0efeaee12bcf154113572
coolabilly <billy.boqvist@elev.ga.ntig.se>
2024-05-27 16:17:59 +0200

    ddd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.net.*;
+import java.sql.SQLOutput;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+        throws Exception {
+        int portnumber = 50000;
+        if (args.length >= 1) {
+            portnumber = Integer.parseInt(args[50000]);
+        }
+
+        MulticastSocket serverMulticastSocket =
+                new MulticastSocket(portnumber);
+        System.out.println("MulticastSocket is created at port " + portnumber);
+
+        InetAdress group =
+                InetAdress.getByName("222.4.5.6");
+
+        serverMulticastSocket.joinGroup(group);
+        System.out.println("joinGroup method is called...");
+        boolean infinite true;
+
+        while (infinite) {
+            byte buf[] = new byte[1024];
+            DatagramPacket data =
+                    new DatagramPacket(buf, buf.length);
+            serverMulticastSocket.receive(data);
+            String msg =
+                    new String(data.getData()).trim();
+            System.out.println("Message received from client = " + msg);
+        }
+        serverMulticastSocket.close();
     }
-}
+    }
+
+
+
+
+
+    }
